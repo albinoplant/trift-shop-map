bf8ec25bc232c4872d0956e28f990818e6307e50
albinoplant <mateuszmalecki@hotmail.com>
2020-10-29 20:11:07 +0100

    up -fighting with heroku

---
+++ client/build/precache-manifest.446d301a3921cf95480ad7b797eff2a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee7f796bea3ccf91aa58719c1627141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51a3cb63dc57bfc07f79",
+    "url": "/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "9c8bd2328ffd11b84d8f",
+    "url": "/static/css/main.85b6c946.chunk.css"
+  },
+  {
+    "revision": "51a3cb63dc57bfc07f79",
+    "url": "/static/js/2.7fb7e80b.chunk.js"
+  },
+  {
+    "revision": "256722dd7b701a66e91d9a4e4a8765ff",
+    "url": "/static/js/2.7fb7e80b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c8bd2328ffd11b84d8f",
+    "url": "/static/js/main.8e08bf25.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "82cfb84681adaf44d42836b3bee574d6",
+    "url": "/static/media/mark-trifto.82cfb846.svg"
+  }
+]);
